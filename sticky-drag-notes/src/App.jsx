d7f4a3f30c6244f4ca2d1fcc5ac69f700d9ca7bb
Geetesh Aheriya <geeteshaheriya506@gmail.com>
2024-11-19 10:17:12 +0530

    third commit

---
+++ sticky-drag-notes/src/App.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react'
+import StickyCard from './Card';
+
+const App = () => {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [cards, setCards] = useState(()=>{
+    const saveCards = localStorage.getItem("stickyNotes");
+    return saveCards ? JSON.parse(saveCards) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("stickyNotes", JSON.stringify(cards));
+  }, [cards]);
+
+
+  const createHandler = (event) => {
+    event.preventDefault();
+
+    if (!title || !details) return; 
+
+    const newCard = {
+      id: Date.now(), 
+      title,
+      details,
+      x: 0,
+      y: 0
+    };
+
+    setCards([...cards, newCard]); 
+    setTitle("");
+    setDetails("");
+  };
+
+  const updateCardsPosition = (id, x, y) => {
+    setCards(cards.map((card)=>
+      card.id == id ? { ...card, x, y} : card
+    ))  
+  }
+
+  const deleteCard = (id) => {
+    setCards(cards.filter((card) => card.id !== id)); 
+  };
+
+  return (
+    <div>
+      <div className="board">
+        <div className="info">
+          <h2 className='heading'>Sticky Notes</h2>
+          <input 
+            className='title'
+            type="text" 
+            placeholder='Enter Topic Title'
+            id='title'
+            value={title}
+            onChange={(e)=>setTitle(e.target.value)}/>
+
+          <textarea 
+            className='textarea'
+            name="stickyinfo" 
+            id="stickyinfo"
+            rows="5"
+            cols="36"
+            value={details}
+            onChange={(e)=>{setDetails(e.target.value)}}></textarea>
+
+          <button 
+            className='submit'
+            onClick={createHandler} >Submit
+            </button>
+        </div>
+        <div className="card">
+        {cards.map((card) => (
+            <StickyCard 
+              key={card.id} 
+              title={card.title} 
+              details={card.details}
+              onDelete={() => deleteCard(card.id)}
+              onDrag={updateCardsPosition} />
+          ))}
+        </div>   
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ sticky-drag-notes/src/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import Draggable from "react-draggable";
+
+const StickyCard = ({ id, title, details, onDelete, onDrag   }) => {
+  const handleStop = (e, data) => {
+    onDrag(id, data.x, data.y)
+  }
+  return (
+    <Draggable onStop={handleStop}bounds="parent" >
+      <div className="note-card">
+        <div className="note-content">
+          <h4 className="note-title">{title}</h4>
+          <p className="note-details">{details}</p>
+        </div>
+        <button className="note-delete" onClick={onDelete}>
+          Delete
+        </button>
+      </div>
+    </Draggable>
+  );
+};
+
+export default StickyCard;
